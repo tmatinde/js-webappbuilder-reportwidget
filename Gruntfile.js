251def965001cb2ce9f0bba40a71396b10c98486
tmatinde <tmatinde@esri.com>
2017-05-15 11:05:34 -0700

    Grunt + widget done

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+﻿
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: [
+                'settings/**/*.js',
+                '*.js'
+            ]
+        },
+        jsonlint: {
+            all: {
+                src: [
+                    'settings/**/*.json',
+                    '*.json'],
+                options: {
+                    format: true,
+                    indent: 2,
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsonlint');
+
+    grunt.registerTask('default', ['jshint', 'jsonlint']);
+};
